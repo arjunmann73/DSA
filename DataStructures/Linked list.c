6647b23d9c5fbc550dd117a8784773bbbcd6294b
Arjun Singh Mann <37778385+arjunmann73@users.noreply.github.com>
2019-07-25 17:06:05 +0800

    Added data structure implementations

---
+++ DataStructures/Linked list.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#define inputint(i) scanf("%d", &i)
+#define inputlli(i) scanf("%lld", &i)
+#define for_i(i,a,b) for(i = a; i<b; i++)
+#define lli long long int
+#define flush scanf("\n")
+
+typedef struct _listnode{
+	int item;
+	struct _listnode *next;
+} ListNode;
+
+typedef struct _linkedlist{
+	int size;
+	ListNode *head;
+} LinkedList;
+
+int insertNode(LinkedList *ll, int index, int value){
+	ListNode *pre, *cur;
+
+	if (ll == NULL || index < 0 || index >= ll->size + 1) // -1 implies failure
+		return -1;
+
+	// If empty list or inserting first node, need to update head pointer
+	if (ll->head == NULL || index == 0){
+		cur = ll->head;
+		ll->head = malloc(sizeof(ListNode));
+		ll->head->item = value;
+		ll->head->next = cur;
+		ll->size++;
+		return 0;
+	}
+
+
+	// Find the nodes before and at the target position
+	// Create a new node and reconnect the links
+	if ((pre = findNode(ll, index - 1)) != NULL){
+		cur = pre->next;
+		pre->next = malloc(sizeof(ListNode));
+		pre->next->item = value;
+		pre->next->next = cur;
+		ll->size++;
+		return 0;
+	}
+
+	return -1;
+}
+
+int removeNode(LinkedList *ll, int index){
+
+	ListNode *pre, *cur;
+
+	// Highest index we can remove is size-1
+	if (ll == NULL || index < 0 || index >= ll->size)
+		return -1;
+
+	// If removing first node, need to update head pointer
+	if (index == 0){
+		cur = ll->head->next;
+		free(ll->head);
+		ll->head = cur;
+		ll->size--;
+
+		return 0;
+	}
+
+	// Find the nodes before and after the target position
+	// Free the target node and reconnect the links
+	if ((pre = findNode(ll, index - 1)) != NULL){
+
+		if (pre->next == NULL)
+			return -1;
+
+		cur = pre->next;
+		pre->next = cur->next;
+		free(cur);
+		ll->size--;
+		return 0;
+	}
+
+	return -1;
+}
+
+ListNode *findNode(LinkedList *ll, int index){
+
+	ListNode *temp;
+
+	if (ll == NULL || index < 0 || index >= ll->size)
+		return NULL;
+
+	temp = ll->head;
+
+	if (temp == NULL || index < 0)
+		return NULL;
+
+	while (index > 0){
+		temp = temp->next;
+		if (temp == NULL)
+			return NULL;
+		index--;
+	}
+
+	return temp;
+}
+
+
+int main(){
+
+    return 0;
+}
